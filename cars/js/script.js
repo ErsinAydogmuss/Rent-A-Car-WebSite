28bee9ac69b113cc7a18210eac683a94c7e63327
Ersin Aydoğmuş <ersinaydogmus.7@gmail.com>
2022-04-09 21:18:57 +0300

    full codes
    full rental cars admin and customers page

---
+++ cars/js/script.js
@@ -0,0 +1,20 @@
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+}
+window.onscroll = () =>{
+    loginForm.classList.remove('active');
+}
+
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn')
+const closeBtn = document.querySelector('#close-btn')
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
